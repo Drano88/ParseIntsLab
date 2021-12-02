e93697e3ec5e330c6cda769c04a661b77a997857
itlabs <itlabs@StudentGrid03.it.farmingdale.edu>
2021-12-02 10:09:59 -0500

    Commit 1

---
+++ ParseIntsLab/src/main/java/com/mycompany/parseintslab/ParseInts.java
@@ -0,0 +1,32 @@
+// ParseInts.java
+//
+// Reads a line of text and prints the integers in the line.
+//
+// ****************************************************************
+ 
+package com.mycompany.parseintslab;
+
+import java.util.Scanner;
+
+
+public class ParseInts {
+    public static void main(String[] args)
+    {
+    int val, sum=0;
+    Scanner scan = new Scanner(System.in);
+    String line;
+    System.out.println("Enter a line of text");
+    Scanner scanLine = new Scanner(scan.nextLine());
+    
+    while (scanLine.hasNext()){
+      try{  
+      val = Integer.parseInt(scanLine.next());
+      sum += val;
+      } // ends try
+      catch(NumberFormatException exception){
+      } //ends catch
+    }
+    System.out.println("The sum of the integers on this line is " +
+    sum);
+    }
+}//end of program
